bd924df81d56be9822771969c95bc19eeef9083c
Nazz <nazz.panasiuk@gmail.com>
2025-03-10 12:39:33 +0200

    Create FollowFlowApplicationTests.java

---
+++ src/test/java/org/example/followflow/FollowFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.followflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FollowFlowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
