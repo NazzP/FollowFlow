b5fe036bd84606758c7738ced3d1e6bb5322e6bb
Nazz <nazz.panasiuk@gmail.com>
2025-03-10 11:52:01 +0200

    Application init

---
+++ src/main/java/org/example/followflow/FollowFlowApplication.java
@@ -0,0 +1,13 @@
+package org.example.followflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FollowFlowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FollowFlowApplication.class, args);
+    }
+
+}
